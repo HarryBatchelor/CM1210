4545085dd381b38d9b9b44acf6311a8fde349fe4
HarryBatchelor <hmbatchelor8@gmail.com>
2019-03-27 11:10:25 +0000

    commit 2

---
+++ Encrypt.java
@@ -0,0 +1,13 @@
+import java.io.*;
+public class Encrypt
+{
+public static void main(String [] args)
+{
+if (args.length != 1)
+{
+System.err.println("One argument expected!");
+System.exit(1);
+}
+// TODO: Complete the program
+}
+}
