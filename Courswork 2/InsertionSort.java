263f8afe33754d1fc0a956aa9fbf1a9053dba652
HarryBatchelor <hmbatchelor8@gmail.com>
2019-04-19 21:18:05 +0100

    End me part 2

--- Courswork 2/InsertionSort.java
+++ Courswork 2/InsertionSort.java
@@ -1,31 +1,27 @@
 import java.io.*;
 import java.util.*;
 public class InsertionSort{
-  public static void main(String[] args) {
-      ArrayList<String> Unsorted = new ArrayList<String>();
-      try{
+  public static void main(String[] args) throws FileNotFoundException {
       Scanner s = new Scanner(new File("GPT2.txt"));
-      while (s.hasNext()){
-          Unsorted.add(s.next());
-      }
+      ArrayList<String> Unsorted = new ArrayList<String>();
+      while (s.hasNextLine())
+          Unsorted.add(s.next().toLowerCase().replaceAll("[^A-Za-z0-9]", ""));
       s.close();
-    }
-    catch(IOException e){
-      System.out.println("Error");
-    }
-  // System.out.println(Unsorted);
-}
-public static void InsertionSort(int[] Unsorted){
 
-  for(int j = 1; j < Unsorted.length; j++){
-    int key = Unsorted[j];
-    int i = j;
-    while(i > 0 && Unsorted[i-1] > key){
-      Unsorted[i] = Unsorted[i-1];
-      i = i - 1;
+      String[] Text = Unsorted.toArray(new String[Unsorted.size()]);
+  for(int i = 1; i < Text.length; i++){
+    String key = Text[i];
+    int j = i - 1;
+    while(j >= 0){
+    if(key.compareTo(Text[j]) > 0 ){
+      break;
     }
-    Unsorted[i] = key;
+    Text[j + 1] = Text[j];
+    j--;
   }
+  Text[j+1] = key;
+}
 
+System.out.println(Text);
 }
 }
