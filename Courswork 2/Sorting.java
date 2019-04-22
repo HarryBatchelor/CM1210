312a7108ae4eee9e6ec4d6cacd742cf4e8e8152f
HarryBatchelor <hmbatchelor8@gmail.com>
2019-04-22 18:26:58 +0100

    Almost done q2

--- Courswork 2/Sorting.java
+++ Courswork 2/Sorting.java
@@ -5,37 +5,37 @@ public class Sorting{
   static int moves;
   public static void main(String[] args) {
     try{
-      List<String> txtFile = enterData("GPT2.txt");
+      List<String> dataFull = Data("GPT2.txt");
 
     for (int i = 100; i <= 1000; i += 100){
 
-    List<String> data = new ArrayList<String>(txtFile.subList(0, i));
+    List<String> data = new ArrayList<String>(dataFull.subList(0, i));
 
     comparisons = 0;
     moves = 0;
 
-    long startInsertion = System.currentTimeMillis();
+    long startInsertion = System.nanoTime();
     List<String> DoneInsertion = InsertionSort(data);
-    long endInsertion = System.currentTimeMillis();
+    long endInsertion = System.nanoTime();
     long durationInsertion = (endInsertion - startInsertion);
     System.out.println("INSERTION SORT");
     System.out.println("Number of words in insertion sort: " + Integer.toString(data.size()));
     System.out.println("Number of Comparisons:" + Integer.toString(comparisons));
     System.out.println("Number of Moves:" + Integer.toString(moves));
-    System.out.println("Time taken:" + Long.toString(durationInsertion) + "milliseconds");
+    System.out.println("Time taken:" + Long.toString(durationInsertion) + " Nano seconds");
 
-    // comparisons = 0;
-    // moves = 0;
-    //
-    // long startMerge = System.currentTimeMillis();
-    // List<String> DoneMerge = mergeSort(data);
-    // long endMerge = System.currentTimeMillis();
-    // long durationMerge = (endMerge - startMerge);
-    // System.out.println("MERGE SORT");
-    // System.out.println("Number of words in Merge sort: " + Integer.toString(data.size()));
-    // System.out.println("Number of Comparisons:" + Integer.toString(comparisons));
-    // System.out.println("Number of Moves:" + Integer.toString(moves));
-    // System.out.println("Time taken:" + Long.toString(durationMerge) + "milliseconds");
+    comparisons = 0;
+    moves = 0;
+
+    long startMerge = System.nanoTime();
+    List<String> DoneMerge = mergeSort(data);
+    long endMerge = System.nanoTime();
+    long durationMerge = (endMerge - startMerge);
+    System.out.println("MERGE SORT");
+    System.out.println("Number of words in Merge sort: " + Integer.toString(data.size()));
+    System.out.println("Number of Comparisons:" + Integer.toString(comparisons));
+    System.out.println("Number of Moves:" + Integer.toString(moves));
+    System.out.println("Time taken:" + Long.toString(durationMerge) + " Nano seconds");
 }
 }
 catch(FileNotFoundException e){
@@ -43,10 +43,11 @@ catch(FileNotFoundException e){
 }
 }
 
-public static List<String> enterData() throws FileNotFoundException{
+public static List<String> Data(String path) throws FileNotFoundException{
   List<String> text = new ArrayList<String>();
   try{
-    Scanner s1 = new Scanner(new File("GPT2.txt"));
+    File fileIn = new File(path);
+    Scanner s1 = new Scanner(fileIn);
     while (s1.hasNext()){
       String word = s1.next().toLowerCase().replaceAll("[^A-Za-z0-9]", "");
       if(!text.contains(text) && word.length() > 3){
@@ -60,18 +61,20 @@ public static List<String> enterData() throws FileNotFoundException{
     return text;
   }
   catch(FileNotFoundException e){
-    System.out.println("File not found");
+    throw e;
   }
 }
 
 
-public static List<String> InsertionSort(List<String> Unsorted) {
-      String[] Words = Unsorted.toArray(new String[Unsorted.size()]);
+public static List<String> InsertionSort(List<String> UnsortedInsertion) {
+      String[] Words = UnsortedInsertion.toArray(new String[UnsortedInsertion.size()]);
   for(int i = 1; i < Words.length; i++){
     String key = Words[i];
     int j = i - 1;
+    comparisons++;
     while(j >= 0){
     if(key.compareTo(Words[j]) > 0 ){
+      moves++;
       break;
     }
     Words[j + 1] = Words[j];
@@ -79,6 +82,64 @@ public static List<String> InsertionSort(List<String> Unsorted) {
   }
   Words[j+1] = key;
 }
+return UnsortedInsertion;
+}
 
+public static List<String> mergeSort(List<String> UnsortedMerge){
+  List<String> text = new ArrayList<String>(UnsortedMerge);
+  if (text.size() <= 1) {
+      comparisons++;
+      return text;
+  }
+
+  List<String> leftBranch = new ArrayList<String>();
+  List<String> rightBranch = new ArrayList<String>();
+  int i = 0;
+  for (String item : text) {
+      comparisons++;
+      if (i < text.size()/2) {
+          leftBranch.add(item);
+          moves++;
+      } else {
+          rightBranch.add(item);
+          moves++;
+      }
+      i++;
+  }
+
+  leftBranch = mergeSort(leftBranch);
+  rightBranch = mergeSort(rightBranch);
+  return merge(leftBranch, rightBranch);
+  }
+
+public static List<String> merge(List<String> leftBranch, List<String> rightBranch) {
+    List<String> merging = new ArrayList<String>();
+    while (leftBranch.size() > 0 && rightBranch.size() > 0) {
+        comparisons++;
+        if (leftBranch.get(0).compareTo(rightBranch.get(0)) < 0) {
+            merging.add(leftBranch.get(0));
+            leftBranch.remove(0);
+            moves++;
+        } else {
+            merging.add(rightBranch.get(0));
+            rightBranch.remove(0);
+            moves++;
+        }
+    }
+
+    while (leftBranch.size() != 0) {
+        comparisons++;
+        merging.add(leftBranch.get(0));
+        leftBranch.remove(0);
+        moves++;
+    }
+
+    while (rightBranch.size() != 0) {
+        comparisons++;
+        merging.add(rightBranch.get(0));
+        rightBranch.remove(0);
+        moves++;
+    }
+    return merging;
 }
 }
